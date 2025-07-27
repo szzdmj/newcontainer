f05031a1d396eb5de59c9d80a1bbbfc2ef8c1df3
szzdmj <chuanfuyin001@gmail.com>
2025-07-27 09:53:40 +0800

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -1,8 +1,15 @@
+export class MyContainer {
+  constructor(state: DurableObjectState, env: any) {}
+  async fetch(request: Request): Promise<Response> {
+    return new Response("This is the container object", { status: 200 });
+  }
+}
+
 export default {
   async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
     const container = env.MY_CONTAINER;
 
-    // Proxy the incoming request to the container
-    return await container.fetch(request);
+    // Forward the request to the container
+    return container.fetch(request);
   }
 };
