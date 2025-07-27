c9e9f4739adf92579137c058c0fb134f2ed652dc
szzdmj <chuanfuyin001@gmail.com>
2025-07-27 10:03:16 +0800

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,13 @@
 import { defineContainer } from '@cloudflare/containers';
 
-export class MyContainer {}
-
-const fetch = defineContainer({
-  container: 'ghcr.io/szzdmj/shenzhou-app',
-});
-
 export default {
-  fetch,
-  MyContainer
+  async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const id = env.MY_CONTAINER.idFromName('single-instance');
+    const stub = env.MY_CONTAINER.get(id);
+    return stub.fetch(req);
+  },
 };
+
+export interface Env {
+  MY_CONTAINER: ReturnType<typeof defineContainer>;
+}
