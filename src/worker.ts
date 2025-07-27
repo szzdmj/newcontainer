80cf1e933262adfcc81c6e2046e98e0bbb340e2a
szzdmj <chuanfuyin001@gmail.com>
2025-07-27 10:29:06 +0800

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,27 @@
+export interface Env {
+  MY_CONTAINER: DurableObjectNamespace;
+}
+
 export class MyContainer {
-  async fetch(request: Request) {
-    return fetch(request);
+  constructor(private state: DurableObjectState, private env: Env) {}
+
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+
+    // 示例：将请求转发到容器中的 Web 服务（FrankenPHP + Caddy）
+    const containerUrl = `http://localhost${url.pathname}${url.search}`;
+    const init: RequestInit = {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    };
+
+    return await fetch(containerUrl, init);
   }
 }
+
+export default {
+  fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return env.MY_CONTAINER.fetch(request);
+  },
+};
